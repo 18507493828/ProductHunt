c676f2c9b4a10910b96d0b9c4809c0278c5d42a7
li diwen <493102216@qq.com>
2026-09-21 14:35:17 +0800

    11

--- client/src/useBuildCatalog.js
+++ client/src/useBuildCatalog.js
@@ -1,14 +1,10 @@
-import { useShallow } from "zustand/react/shallow";
 import { useBuildCatalogStore } from "./stores/buildCatalogStore";
 import "./buildConfig";
 
 /** 订阅运营后台下发的场景 / 工具 / 云部署目录（界面结构不变） */
 export default function useBuildCatalog() {
-  return useBuildCatalogStore(
-    useShallow((state) => ({
-      scenes: state.scenes,
-      tools: state.tools,
-      deploys: state.deploys,
-    })),
-  );
+  const scenes = useBuildCatalogStore((state) => state.scenes);
+  const tools = useBuildCatalogStore((state) => state.tools);
+  const deploys = useBuildCatalogStore((state) => state.deploys);
+  return { scenes, tools, deploys };
 }
